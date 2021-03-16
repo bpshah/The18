aeaae8eb57a4bc642d2ffe09fe46c6d48bf1f0d0
bpshah <sbhumit98@gmail.com>
2021-03-16 11:13:01 -0500

    first commit

---
+++ models/teams.js
@@ -0,0 +1,56 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// Create Schema
+var teams = new Schema({
+    sr: {
+        type: Number,
+        required: true
+    },
+    id: {
+        type: Number,
+        required: true
+    },
+    league: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    founded: {
+        type: String,
+        required: true
+    },
+    logo: {
+        type: String,
+        required: true
+    },
+    venue: {
+        type: String,
+        required: true
+    },
+    "venue surface": {
+        type: String,
+        required: true
+    },
+    "venue address": {
+        type: String,
+        required: true
+    },
+    "venue city": {
+        type: String,
+        required: true
+    },
+    "venue capcity": {
+        type: String,
+        required: true
+    }
+});
+
+mongoose.model('teams', teams);
